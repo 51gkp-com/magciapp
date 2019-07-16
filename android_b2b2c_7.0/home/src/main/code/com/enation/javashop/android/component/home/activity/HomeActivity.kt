19ffb98c5c04ed4b50cb83ce30708c58327ee693
wangpos <renhongge@weipass.cn>
2019-07-16 10:10:32 +0800

    放开检测更新

--- android_b2b2c_7.0/home/src/main/code/com/enation/javashop/android/component/home/activity/HomeActivity.kt
+++ android_b2b2c_7.0/home/src/main/code/com/enation/javashop/android/component/home/activity/HomeActivity.kt
@@ -90,7 +90,7 @@ class HomeActivity : BaseActivity<HomeActivityPresenter,HomeActLayBinding>(),Hom
             self.withViewPager(mViewBinding.homeActViewpager)
         }
 //        presenter.isLogin()
-//        presenter.checkUpdate()
+        presenter.checkUpdate()
     }
 
     /**
